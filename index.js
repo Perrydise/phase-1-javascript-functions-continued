e856b1502707033e962f70d0772a8e3122cecc3e
Perry Jorss <pjorss123@gmail.com>
2022-04-07 17:19:37 -0400

    commit tree 438b033006be9438b2cc511a5876a033d0f82157

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(job = "go to the office"){
+    return `This Monday, I will ${job}.`
+}
+
+function wrapAdjective(adj = '*'){
+    const greeting = 'You are ';
+    return function(endGreeting = 'a hard worker'){
+        return `${greeting}${adj}${endGreeting}${adj}!`
+        }
+    }
+wrapAdjective('||')("a dedicated programmer");
